917ab783feb4f63d8ebf378d41a03877df51bbe5
divyansh2105 <41578691+divyansh2105@users.noreply.github.com>
2019-04-11 23:09:31 +0530

    Add files via upload

---
+++ SampleProject/MyApp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from django.core import serializers
+from django.conf import settings
+import json
+# Create your views here.
+@api_view(["POST"])
+def IdealWeight(heightdata):
+    if heightdata.method == 'POST':
+        height=json.loads(heightdata.body)
+        weight=str(height*10)
+        return JsonResponse("Ideal weight should be:"+weight+" kg",safe=False)
+    return JsonResponse("Get method",safe=False)
+    
